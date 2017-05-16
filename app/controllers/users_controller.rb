001fee40f3b2ba0401dc8097caf9b7a76e8e2184
thomcorley <tom.corley@freeagent.com>
2017-05-16 13:55:59 +0100

    fixed the login for primary user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -54,12 +54,15 @@ class UsersController < ApplicationController
       }
     end
 
+    email = user_params["email_address"]
 
-    p @user = User.new(user_params)
+    ap @user = User.new(user_params)
 
-    if User.where(:id => @user.id).exists?
-      redirect_to "/users/#{@user.id}"
-    elsif @user.save
+    if User.where(email_address: email).exists?
+      user = User.find_by_email_address email
+      redirect_to "/users/#{user.id}"
+    elsif
+      @user.save
       redirect_to "/users/#{@user.id}"
     end
 
