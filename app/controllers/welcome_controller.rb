45c591abf95a6ef57dbbbdff7cc6f7ffeff9215e
thomcorley <thomcorley@gmail.com>
2017-05-18 07:56:28 +0100

    changed redirect_uri_

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -2,9 +2,7 @@ class WelcomeController < ApplicationController
   def index
     @client_id = "85887f1a18dd3c5d43fe"
     @client_secret = "28d5ed3ad4f614802ed419322c487e2d3329b342"
-    @redirect_uri = "http://localhost:3000/intro"
-    # SecureRandom.hex
-    # SecureRandom.base64
+    @redirect_uri = "https://intense-sierra-15257.herokuapp.com/intro"
     @state = "6b41778eec044a860b93f204e3418ded"
     @allow_signup = false
   end
