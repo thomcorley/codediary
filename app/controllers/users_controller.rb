79d7d8f946c76755b9e267bf905818a5d139a697
thomcorley <thomcorley@gmail.com>
2017-05-18 20:00:09 +0100

    created dynamic greeting

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,7 +9,7 @@ class UsersController < ApplicationController
 
   def show
     @user = User.find(params[:id])
-    @entries
+    @entries = @user.entries
 
     now = DateTime.now
 
@@ -26,7 +26,7 @@ class UsersController < ApplicationController
   def intro
     @client_id = "85887f1a18dd3c5d43fe"
     @client_secret = "28d5ed3ad4f614802ed419322c487e2d3329b342"
-    @redirect_uri = "http://www.codediary.co/intro"
+    @redirect_uri = "http://localhost:3000/intro"
     @state = "6b41778eec044a860b93f204e3418ded"
     @allow_signup = false
 
