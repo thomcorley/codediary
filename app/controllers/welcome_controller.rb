251848b5395e9b90eaf3d9746bb212dd5063eb1e
thomcorley <thomcorley@gmail.com>
2017-05-18 20:59:42 +0100

    updated redirect uri

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -2,7 +2,7 @@ class WelcomeController < ApplicationController
   def index
     @client_id = "85887f1a18dd3c5d43fe"
     @client_secret = "28d5ed3ad4f614802ed419322c487e2d3329b342"
-    @redirect_uri = "http://localhost:3000/intro"
+    @redirect_uri = "http://www.codediary.co/intro"
     @state = "6b41778eec044a860b93f204e3418ded"
     @allow_signup = false
   end
