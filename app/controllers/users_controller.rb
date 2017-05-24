281b1924b397410dae5529cd79a7dda61417fbc0
Tom Corley <thomcorley@gmail.com>
2017-05-24 08:11:53 +0100

    changed redirect uri

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -124,7 +124,7 @@ class UsersController < ApplicationController
 
     @client_id = "794263904785-vea01ahfrk7glbtclgmu384tqvbsid5d.apps.googleusercontent.com"
     @client_secret = "R-Yvu-RIXpQ3HT0EKbQJ-RMl"
-    @redirect_uri = "http://www.codediary.co/intro"
+    @redirect_uri = "http://localhost:3000/intro"
 
     ap code = params["code"]
 
