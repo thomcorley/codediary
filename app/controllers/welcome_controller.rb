4d59c654eb8499b18119e6638e3ea8f7bedf2294
Tom Corley <thomcorley@gmail.com>
2017-05-24 17:52:57 +0100

    edited searches_controller

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,9 +1,7 @@
 class WelcomeController < ApplicationController
   def index
-
     @client_id = "794263904785-vea01ahfrk7glbtclgmu384tqvbsid5d.apps.googleusercontent.com"
     @client_secret = "R-Yvu-RIXpQ3HT0EKbQJ-RMl"
     @redirect_uri = "http://localhost:3000/intro"
-
   end
 end
