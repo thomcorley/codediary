09406fae5accfb79ff32a61da66eb08078cbe420
Tom Corley <thomcorley@gmail.com>
2017-05-25 18:47:11 +0100

    added ENV variables

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -32,7 +32,7 @@ class UsersController < ApplicationController
 
     @client_id = "794263904785-vea01ahfrk7glbtclgmu384tqvbsid5d.apps.googleusercontent.com"
     @client_secret = "R-Yvu-RIXpQ3HT0EKbQJ-RMl"
-    @redirect_uri = "http://localhost:3000/intro"
+    @redirect_uri = "#{ENV["HOST"]}/intro"
 
     ap code = params["code"]
 
